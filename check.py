a718346f2becf84c5f66f5e45982cc6c0cdcc832
sigseg5 <sigseg5@protonmail.com>
2023-03-13 09:38:40 +0400

    Update russian ca list

--- check.py
+++ check.py
@@ -25,7 +25,7 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
 }
 
-untrusted = ["Russian Trusted", "SberCA", "St. Petersburg", "VTB Group"]
+untrusted = ["Russian Trusted", "SberCA", "St. Petersburg", "VTB Group", "Bank GPB", "Администрация Партизанского городского округа", "Kaliningrad", "Sigma-REZERV"]
 
 
 def check_link(link, index):
