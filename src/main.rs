59bbe4b589b394d72536513a17b180f87ae16805
sigseg5 <sigseg5@protonmail.com>
2023-04-22 20:49:07 +0400

    Del control-c handler

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,10 @@
 use std::path::Path;
-use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::Arc;
 
 mod common;
 mod consts;
 mod ssl;
 
 fn main() {
-    let running: Arc<AtomicBool> = Arc::new(AtomicBool::new(true));
-    let r: Arc<AtomicBool> = running.clone();
-
-    // Set control-c handler
-    ctrlc::set_handler(move || {
-        r.store(false, Ordering::SeqCst);
-    })
-    .expect("Error setting Ctrl-C handler");
-
     let gov_path = Path::new("dataset/government_domains_latest.txt");
     let gov_contents_result = common::read_file_lines(gov_path);
     let gov_contents = match gov_contents_result {
@@ -23,21 +12,20 @@ fn main() {
         Err(error) => panic!("Problem opening the file: {:?}", error),
     };
 
-    while running.load(Ordering::SeqCst) {
-        for url in gov_contents.iter() {
-            let info_result = ssl::get_issuer_for(url);
-            let res = match info_result {
-                Ok(info) => info,
-                Err(error) => {
-                    format!("Error getting the issuer for {}, err: {}", url, error);
-                    continue;
-                }
-            };
-            let url_stats: Vec<String> = res.split(';').map(String::from).collect();
-            if url_stats[1].contains("Russian") {
-                println!("{} – {}", url, url_stats[1])
+    for url in gov_contents.iter() {
+        let info_result = ssl::get_issuer_for(url);
+        let res = match info_result {
+            Ok(info) => info,
+            Err(error) => {
+                format!("Error getting the issuer for {}, err: {}", url, error);
+                continue;
             }
+        };
+        let url_stats: Vec<String> = res.split(';').map(String::from).collect();
+        if url_stats[1].contains("Russian Trusted") {
+            println!("{} – {}", url, url_stats[1])
+        } else {
+            println!("{} skipped", url)
         }
-        break;
     }
 }
